a34e36abc6118db2b7dd2c1047b58c00cbbb77cf
thangzathang <thangzathang@gmail.com>
2021-05-31 13:38:07 +1000

    Intial commit and add root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+    def home
+        # What ever the def action here is, a html.erb is expected
+        # You dont put html here - that is for VIEWS!
+            # VIEWS - All html and css
+        # You pass stuff down to the html here! 
+            # CONTROLLER - Data and objects sent
+    end
+
+end
